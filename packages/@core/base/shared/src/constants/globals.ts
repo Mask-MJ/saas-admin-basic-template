affa45e307c5421da0998559921216143c6fb94b
mask <momo617473294@gmail.com>
2024-12-10 00:17:20 +0800

    feat: add core

---
+++ packages/@core/base/shared/src/constants/globals.ts
@@ -0,0 +1,13 @@
+/** layout content 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_CONTENT_HEIGHT = `--saas-content-height`;
+/** layout content 组件的宽度 */
+export const CSS_VARIABLE_LAYOUT_CONTENT_WIDTH = `--saas-content-width`;
+/** layout header 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_HEADER_HEIGHT = `--saas-header-height`;
+/** layout footer 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_FOOTER_HEIGHT = `--saas-footer-height`;
+
+/**
+ * @zh_CN 默认命名空间
+ */
+export const DEFAULT_NAMESPACE = 'saas';
---
+++ packages/@core/base/shared/src/constants/saas.ts
@@ -0,0 +1,23 @@
+/**
+ * @zh_CN GITHUB 仓库地址
+ */
+export const SAAS_GITHUB_URL =
+  'https://github.com/masj-mj/saas-admin-basic-template';
+
+/**
+ * @zh_CN 文档地址
+ */
+export const SAAS_DOC_URL = 'https://doc.saas.pro';
+
+/**
+ * @zh_CN Saas Logo
+ */
+export const SAAS_LOGO_URL =
+  'https://unpkg.com/@vbenjs/static-source@0.1.7/source/logo-v1.webp';
+
+/**
+ * @zh_CN Saas Admin 首页地址
+ */
+export const SAAS_PREVIEW_URL = 'https://www.saas.pro';
+
+export const SAAS_NAIVE_PREVIEW_URL = 'https://naive.saas.pro';
